fba26d06e4f22247b09d22206a3c0195675d3078
AlexDavid26 <82370626+AlexDavid26@users.noreply.github.com>
2022-08-26 12:34:22 +0300

    Added shop background

--- src/pages/shop/shop.js
+++ src/pages/shop/shop.js
@@ -12,74 +12,76 @@ import style from "./shop.module.scss";
 
 const Shop = () => {
     return (
-        <div className={style.shop}>
-            <div className={style.title}>
-                <h2 className={style.text}>Shop</h2>
-                <img src="/icons/shop.png" alt="shop icon" onClick={() => window.location.href="/shop/cart"}/>
+        <div>
+            <div className={style.shop}>
+                <div className={style.title}>
+                    <h2 className={style.text}>Shop</h2>
+                    <img src="/icons/shop.png" alt="shop icon" onClick={() => window.location.href="/shop/cart"}/>
+                </div>
+                <div className={style.items}>
+                    <Row>
+                        <Col>
+                            <ShopItem 
+                                image="yes" 
+                                imgSrc="/images/shop-images/1.png"
+                                text="Sensor Package 1"
+                                href="/sensor-package-1"
+                                className={style.item}
+                            />
+                        </Col>
+                        <Col>
+                            <ShopItem 
+                                image="yes" 
+                                imgSrc="/images/shop-images/2.png"
+                                text="Sensor Package 2"
+                                href="/sensor-package-2"
+                                className={style.item}
+                            />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <ShopItem 
+                                image="yes" 
+                                imgSrc="/images/shop-images/3.png"
+                                text="Sensor Package 3"
+                                href="/sensor-package-3"
+                                className={style.item}
+                            />
+                        </Col>
+                        <Col>
+                            <ShopItem 
+                                image="yes" 
+                                imgSrc="/images/shop-images/4.png"
+                                text="Sensor Package 4"
+                                href="/sensor-package-4"
+                                className={style.item}
+                            />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <ShopItem 
+                                image="yes" 
+                                imgSrc="/images/shop-images/5.png"
+                                text="Sensor Package 5"
+                                href="/sensor-package-5"
+                                className={style.item}
+                            />
+                        </Col>
+                        <Col>
+                            <ShopItem 
+                                image="yes" 
+                                imgSrc="/images/shop-images/6.png"
+                                text="Sensor Package 6"
+                                href="/sensor-package-6"
+                                className={style.item}
+                            />
+                        </Col>
+                    </Row>
+                </div>      
             </div>
-            <div className={style.items}>
-                <Row>
-                    <Col>
-                        <ShopItem 
-                            image="yes" 
-                            imgSrc="/images/shop-images/1.png"
-                            text="Sensor Package 1"
-                            href="/sensor-package-1"
-                            className={style.item}
-                        />
-                    </Col>
-                    <Col>
-                        <ShopItem 
-                            image="yes" 
-                            imgSrc="/images/shop-images/2.png"
-                            text="Sensor Package 2"
-                            href="/sensor-package-2"
-                            className={style.item}
-                        />
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <ShopItem 
-                            image="yes" 
-                            imgSrc="/images/shop-images/3.png"
-                            text="Sensor Package 3"
-                            href="/sensor-package-3"
-                            className={style.item}
-                        />
-                    </Col>
-                    <Col>
-                        <ShopItem 
-                            image="yes" 
-                            imgSrc="/images/shop-images/4.png"
-                            text="Sensor Package 4"
-                            href="/sensor-package-4"
-                            className={style.item}
-                        />
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <ShopItem 
-                            image="yes" 
-                            imgSrc="/images/shop-images/5.png"
-                            text="Sensor Package 5"
-                            href="/sensor-package-5"
-                            className={style.item}
-                        />
-                    </Col>
-                    <Col>
-                        <ShopItem 
-                            image="yes" 
-                            imgSrc="/images/shop-images/6.png"
-                            text="Sensor Package 6"
-                            href="/sensor-package-6"
-                            className={style.item}
-                        />
-                    </Col>
-                </Row>
-            </div>
-            <Footer className={style.footer}/>        
+            <Footer/>  
         </div>
     )
 }
