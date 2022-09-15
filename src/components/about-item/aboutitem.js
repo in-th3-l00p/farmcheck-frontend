c2d3c9d2b97c4e6d4c007ec44dd978e8b05105da
AlexDavid26 <82370626+AlexDavid26@users.noreply.github.com>
2022-09-16 00:16:36 +0300

    About page (not finished)
    needs some adjustments

---
+++ src/components/about-item/aboutitem.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+
+import style from "./aboutitem.module.scss";
+
+const AboutItem = ({ imageSrc, name, description }) => {
+    return (
+        <div className={style.container}>
+            <img src={imageSrc} alt={name} />
+            <h3 className={style.name}>{name}</h3>
+            <h5>{description}</h5>
+        </div>
+    );
+};
+
+export default AboutItem;
