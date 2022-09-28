0ecf1449ed47c86635d09a914373032a08131c26
eduse <edu.serban26@gmail.com>
2022-09-28 11:32:03 +0300

    Modifiend Proxy URL. Potential fix

--- src/index.js
+++ src/index.js
@@ -6,5 +6,12 @@ import axios from "axios";
 // proxy the axios
 axios.defaults.baseURL = "https://farmcheck-test.herokuapp.com";
 
+const proxy = require('http-proxy-middleware')
+
+module.exports = function(app) {
+    // add other server routes to path array
+    app.use(proxy(['/api' ], { target: 'https://farmcheck-test.herokuapp.com' }));
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
