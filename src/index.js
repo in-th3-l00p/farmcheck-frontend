a505e9b16c03ec92f2c84a620252d563aa7f2982
eduse <edu.serban26@gmail.com>
2022-09-28 11:36:12 +0300

    Reverting changes

--- src/index.js
+++ src/index.js
@@ -6,12 +6,5 @@ import axios from "axios";
 // proxy the axios
 axios.defaults.baseURL = "https://farmcheck-test.herokuapp.com";
 
-const proxy = require('http-proxy-middleware')
-
-module.exports = function(app) {
-    // add other server routes to path array
-    app.use(proxy(['/api' ], { target: 'https://farmcheck-test.herokuapp.com' }));
-}
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
