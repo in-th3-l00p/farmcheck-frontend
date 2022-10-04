c2e162d7297913f2ffbb6e984657f10eef9faa8d
AlexDavid26 <82370626+AlexDavid26@users.noreply.github.com>
2022-10-04 21:01:21 +0300

    Styling of create task

--- src/pages/auth/auth.js
+++ src/pages/auth/auth.js
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import TextBox from "../../components/textbox/textbox";
-import { CountryInput, InputRange, LabelInput, PhoneNumberInput } from "../../components/forms/forms";
+import {
+    CountryInput,
+    InputRange,
+    LabelInput,
+    PhoneNumberInput,
+} from "../../components/forms/forms";
 import { Alert } from "react-bootstrap";
 import ErrorAlert from "../../components/alerts/error";
-import { Button } from "../../components/buttons/buttons"; 
+import { Button } from "../../components/buttons/buttons";
 
 import style from "./auth.module.scss";
 import { storeJWT } from "../../lib/auth";
@@ -16,18 +21,13 @@ import { storeJWT } from "../../lib/auth";
 const DaysExceptions = (month, year) => {
     const evenMonths = "4, 6, 9, 11";
 
-    if (month === "")
-        return 31;
-    else if (evenMonths.includes(month))
-        return 30;
+    if (month === "") return 31;
+    else if (evenMonths.includes(month)) return 30;
     else if (month === "2") {
-        if (parseInt(year) % 4 === 0)
-            return 29;
-        return 28; 
-    }
-    else
-        return 31;
-}
+        if (parseInt(year) % 4 === 0) return 29;
+        return 28;
+    } else return 31;
+};
 
 /**
  * Login page.
@@ -40,7 +40,7 @@ export const Login = () => {
 
     const [error, setError] = useState(""); // auth error
     const [registered, setRegistered] = useState(
-        (new URLSearchParams(window.location.search)).get("registered")
+        new URLSearchParams(window.location.search).get("registered")
     );
 
     return (
@@ -49,20 +49,18 @@ export const Login = () => {
                 className="form"
                 onSubmit={(event) => {
                     event.preventDefault();
-                    axios.post(
-                        "/api/authenticate",
-                        {
-                            "username": username,
-                            "password": password
-                        }
-                    )   
-                        .then(resp => {
+                    axios
+                        .post("/api/authenticate", {
+                            username: username,
+                            password: password,
+                        })
+                        .then((resp) => {
                             if (resp.status === 200) {
                                 storeJWT(resp.data.id_token);
                                 window.location.href = "/";
                             }
                         })
-                        .catch(err => {
+                        .catch((err) => {
                             switch (err.code) {
                                 case "ERR_BAD_REQUEST":
                                     setError(err.response.data.detail);
@@ -71,7 +69,7 @@ export const Login = () => {
                                     setError("Server error");
                                     break;
                             }
-                        })
+                        });
                 }}
             >
                 <h2 className="text-center mb-4">Login</h2>
@@ -83,19 +81,19 @@ export const Login = () => {
                         onClose={() => setRegistered(null)}
                         dismissible
                     >
-                        Your account was registered. Activate your account using the mail
-                        sent to your email address
+                        Your account was registered. Activate your account using
+                        the mail sent to your email address
                     </Alert>
                 )}
                 <ErrorAlert error={error} setError={setError} />
 
                 <span className="input-container">
                     <LabelInput
-                        label="Username" 
+                        label="Username"
                         value={username}
                         setValue={setUsername}
                     />
-                    <LabelInput 
+                    <LabelInput
                         label="Password"
                         type="password"
                         value={password}
@@ -103,29 +101,26 @@ export const Login = () => {
                     />
 
                     <Alert variant="warning" className={style.alert}>
-                        Don't have an account. 
-                        <Alert.Link 
-                            href="/register" 
-                            className={style.link}
-                        >
+                        Don't have an account.
+                        <Alert.Link href="/register" className={style.link}>
                             Register here
                         </Alert.Link>
                     </Alert>
                 </span>
 
                 <div className="d-flex justify-content-center gap-2">
-                    <Button 
-                        type="submit" 
-                        disabled={!username || !password} 
-                        style={{width: "150px"}}
+                    <Button
+                        type="submit"
+                        disabled={!username || !password}
+                        style={{ width: "150px", fontWeight: 500 }}
                     >
                         Login
                     </Button>
                 </div>
             </form>
         </TextBox>
-    )
-}
+    );
+};
 
 /**
  * Register page.
@@ -137,21 +132,21 @@ export const Register = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
-    const [name, setName] = useState({first: "", last: ""});
+    const [name, setName] = useState({ first: "", last: "" });
     const [country, setCountry] = useState("");
     const [phoneNumber, setPhoneNumber] = useState("");
-    const [dob, setDOB] = useState({day: "", month: "", year: ""});
+    const [dob, setDOB] = useState({ day: "", month: "", year: "" });
 
     const [error, setError] = useState("");
 
     // checks if all fields are completed
     const isInputValid = () => {
         return (
-            !!email && 
-            !!username && 
-            !!password && 
-            password === confirmPassword && 
-            !!name.first && 
+            !!email &&
+            !!username &&
+            !!password &&
+            password === confirmPassword &&
+            !!name.first &&
             !!name.last &&
             !!country &&
             !!phoneNumber &&
@@ -159,44 +154,46 @@ export const Register = () => {
             !!dob.month &&
             !!dob.year
         );
-    }
+    };
 
     // deal with month or year changing
     useEffect(() => {
         const maxDay = DaysExceptions(dob.month, dob.year);
-        if (dob.day > maxDay)
-            setDOB({...dob, day: maxDay});
-    }, [dob])
+        if (dob.day > maxDay) setDOB({ ...dob, day: maxDay });
+    }, [dob]);
 
     return (
         <TextBox className="form-container">
-            <form className="form" onSubmit={(event) => {
-                event.preventDefault();
-                axios.post("/api/register", {
-                    "login": username,
-                    "firstName": name.first,
-                    "lastName": name.last,
-                    "email": email,
-                    "password": password,
-                    "phoneNumber": phoneNumber,
-                    "langKey": "en"
-                })
-                    .then(resp => { 
-                        if (resp.status === 201)
-                            window.location.href = "/login?registered"
-                    })
-                    .catch(err => {
-                        switch (err.code) {
-                            case "ERR_BAD_REQUEST":
-                                setError(err.response.data.title);
-                                break;
-                            default:
-                                setError("Server error");
-                                break;
-                        }
-                    })
-
-            }}>
+            <form
+                className="form"
+                onSubmit={(event) => {
+                    event.preventDefault();
+                    axios
+                        .post("/api/register", {
+                            login: username,
+                            firstName: name.first,
+                            lastName: name.last,
+                            email: email,
+                            password: password,
+                            phoneNumber: phoneNumber,
+                            langKey: "en",
+                        })
+                        .then((resp) => {
+                            if (resp.status === 201)
+                                window.location.href = "/login?registered";
+                        })
+                        .catch((err) => {
+                            switch (err.code) {
+                                case "ERR_BAD_REQUEST":
+                                    setError(err.response.data.title);
+                                    break;
+                                default:
+                                    setError("Server error");
+                                    break;
+                            }
+                        });
+                }}
+            >
                 <h2 className="text-center mb-4">Register</h2>
                 <ErrorAlert error={error} setError={setError} />
 
@@ -209,118 +206,112 @@ export const Register = () => {
                         setValue={setEmail}
                     />
 
-                    <LabelInput 
-                        label="Username" 
+                    <LabelInput
+                        label="Username"
                         value={username}
                         setValue={setUsername}
                     />
 
                     <div className="d-flex gap-2">
-                        <LabelInput 
+                        <LabelInput
                             label="First name"
                             className="w-100"
                             value={name.first}
-                            setValue={(newName) => setName(
-                                {...name, first: newName}
-                            )}
+                            setValue={(newName) =>
+                                setName({ ...name, first: newName })
+                            }
                         />
 
-                        <LabelInput 
+                        <LabelInput
                             label="Last name"
                             className="w-100"
                             value={name.last}
-                            setValue={(newName) => setName(
-                                {...name, last: newName}
-                            )}
+                            setValue={(newName) =>
+                                setName({ ...name, last: newName })
+                            }
                         />
                     </div>
                 </span>
 
                 {/* password */}
                 <span className="input-container">
-                    <LabelInput 
-                        type="password" 
+                    <LabelInput
+                        type="password"
                         label="Password"
                         value={password}
                         setValue={setPassword}
                     />
 
-                    <LabelInput 
-                        type="password" 
+                    <LabelInput
+                        type="password"
                         label="Confirm password"
                         value={confirmPassword}
                         setValue={setConfirmPassword}
                     />
 
-                    {password !== confirmPassword && 
+                    {password !== confirmPassword && (
                         <Alert variant="danger" className="mb-0">
                             Passwords don't match.
                         </Alert>
-                    }
+                    )}
                 </span>
 
                 {/* date of birth and country */}
                 <span className="input-container">
-                    <CountryInput 
-                        value={country} 
-                        setValue={setCountry} 
-                    />
-                    <PhoneNumberInput 
-                        value={phoneNumber} 
-                        setValue={setPhoneNumber} 
+                    <CountryInput value={country} setValue={setCountry} />
+                    <PhoneNumberInput
+                        value={phoneNumber}
+                        setValue={setPhoneNumber}
                     />
 
                     <div className="d-flex gap-2">
-                        <InputRange 
+                        <InputRange
                             label="Day"
                             className="w-100"
                             maximum={DaysExceptions(dob.month, dob.year)}
                             value={dob.day}
                             setValue={(newValue) => {
-                                setDOB({...dob, day: newValue});
+                                setDOB({ ...dob, day: newValue });
                             }}
                         />
-                        <InputRange 
+                        <InputRange
                             label="Month"
                             className="w-100"
                             maximum={12}
                             value={dob.month}
                             setValue={(newValue) => {
-                                setDOB({...dob, month: newValue});
+                                setDOB({ ...dob, month: newValue });
                             }}
                         />
-                        <InputRange 
+                        <InputRange
                             label="Year"
                             className="w-100"
                             maximum={Number.MAX_VALUE}
                             value={dob.year}
                             setValue={(newValue) => {
-                                setDOB({...dob, year: newValue});
+                                setDOB({ ...dob, year: newValue });
                             }}
                         />
                     </div>
 
                     <Alert variant="warning" className={style.alert}>
-                        Already have an account. 
-                        <Alert.Link 
-                            href="/login" 
-                            className={style.link}
-                        >
+                        Already have an account.
+                        <Alert.Link href="/login" className={style.link}>
                             Login here
                         </Alert.Link>
                     </Alert>
                 </span>
 
                 <span className="d-flex justify-content-center">
-                    <Button 
+                    <Button
                         type="submit"
                         disabled={!isInputValid()}
-                        style={{width: "150px"}}
+                        style={{ width: "150px", fontWeight: 500 }}
                     >
                         Submit
                     </Button>
                 </span>
             </form>
         </TextBox>
-    )
-}
+    );
+};
