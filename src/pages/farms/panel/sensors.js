674301c9edc0989b275f7362edf83c8af4a7ddad
in-th3-l00p <indianucode0303@gmail.com>
2022-11-07 21:30:47 +0200

    bonjour

--- src/pages/farms/panel/sensors.js
+++ src/pages/farms/panel/sensors.js
@@ -2,7 +2,9 @@ import { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
 import { Button } from "../../../components/buttons/buttons";
 import ErrorAlert from "../../../components/alerts/error";
+import { LabelInput, LabelTextInput } from "../../../components/forms/forms";
 import sensorService from "../../../lib/services/sensorService";
+import { useParams } from "react-router-dom";
 
 import style from "../styles/panel.module.scss";
 
@@ -15,6 +17,76 @@ const SensorsTabPlaceholder = () => {
         <Container>
             <p>loading...</p>
         </Container>
+    );
+};
+
+/**
+ * Page component at "/sensors/create/:farm-id". Used for creating a new component.
+ * @return {JSX.Element} the page component
+ */
+export const CreateSensor = () => {
+    const params = useParams();
+
+    const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
+
+    const [error, setError] = useState("");
+
+    if (!params["farm_id"])
+        return (
+            <div
+                className={"d-flex align-items-center justify-content-center"}
+                style={{ width: "100vw", height: "100vh" }}
+            >
+                <h1>
+                    You need to provide a id for the farm where the sensor will
+                    be added
+                </h1>
+            </div>
+        );
+
+    return (
+        <div className={`${style.textBox} form-container`}>
+            <form
+                className="form"
+                onSubmit={(event) => {
+                    event.preventDefault();
+                    sensorService
+                        .createSensor(params["farm_id"], name, description)
+                        .then(
+                            () =>
+                                (window.location.href = `/farms/panel/${params["farm_id"]}?sensorAdded`)
+                        )
+                        .catch((err) => setError(err.message));
+                }}
+            >
+                <h2 className="mb-4 text-center">Create sensor</h2>
+                {error && <ErrorAlert error={error} setError={setError} />}
+                <span className="input-container">
+                    <LabelInput
+                        label="Sensor name:"
+                        value={name}
+                        setValue={setName}
+                    />
+                    <LabelTextInput
+                        label="Sensor description:"
+                        inputStyle={{ height: "200px" }}
+                        value={description}
+                        setValue={setDescription}
+                    />
+                </span>
+
+                <span className="d-flex justify-content-center">
+                    <Button
+                        type="submit"
+                        className={style.button}
+                        disabled={!name.length}
+                    >
+                        Submit
+                    </Button>
+                </span>
+            </form>
+        </div>
     );
 };
 
